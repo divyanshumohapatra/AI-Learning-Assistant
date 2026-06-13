474e533cbc110b8ebb32d6f0898075b36f58d383
Divyanshu Mohapatra <divyanshu@Divyanshus-MacBook-Air.local>
2026-06-13 23:12:59 +0530

    Add flashcard routes and api

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ import connectDB from './config/db.js';
 
 import authRoutes from './routes/authRoutes.js'
 import documentRoutes from './routes/documentRoutes.js'
+import flashcardRoutes from './routes/flashcardRoutes.js'
 
 // ES6 module __dirname alternative
 const __filename = fileURLToPath(import.meta.url);
@@ -43,6 +44,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // Routes
 app.use('/api/auth', authRoutes)
 app.use('/api/documents', documentRoutes)
+app.use('/api/flashcards', flashcardRoutes)
 
 // 404 Handler
 app.use((req, res) => {
